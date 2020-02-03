1fb205c8091542a485a9c4e5dff954a6b7a9dc10
sotmazgi <shonOtmazgin>
2020-02-03 10:30:01 +0200

    adding package files

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from version import get_package_version
+
+setup(
+    name='route_optimization',
+    version=get_package_version(),
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/shon-otmazgin/route_optimization',
+    license='MIT License',
+    author='Sapir Rubin;Shon Otmazgin',
+    author_email='rubinsapir@gmail.com;shon711@gmail.com',
+    description='route_optimization',
+    python_requires='>=3.6',
+    install_requires=[
+
+    ]
+)
---
+++ version.py
@@ -0,0 +1,5 @@
+version = '1.0.0.0'
+
+
+def get_package_version():
+    return version
